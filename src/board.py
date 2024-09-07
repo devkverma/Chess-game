38b3dff5b44db41d480e24d7fb81b8ae9541fdf7
devkverma <devverma269@gmail.com>
2024-09-07 11:29:28 +0530

    Updated board.py

--- src/board.py
+++ src/board.py
@@ -8,10 +8,8 @@ class Board:
 
     def _create(self):
         self.squares = np.zeros((8,8))
-        print(self.squares)
+        
 
     def _add_pieces(self,color):
         pass
 
-b = Board()
-b._create()
